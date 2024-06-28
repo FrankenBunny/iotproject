a1778547943fff7719ee0dd2aa9085b5bb7a36e7
Julia Johansson <juliajsson@hotmail.com>
2024-06-28 10:32:34 +0200

    Updated so that the machine sleeps if internet connection is not established

--- boot.py
+++ boot.py
@@ -1,24 +1,29 @@
 # boot.py -- run on boot-up
+import machine
 import keys
 import network
 from time import sleep
 
 def connect():
+
+    # Reset connection
+    wlan.active(False)                          # Deactivate network interface
+    wlan.active(True)                           # Activate network interface
+
     wlan = network.WLAN(network.STA_IF)         # Put modem on Station mode
     if not wlan.isconnected():                  # Check if already connected
         print('connecting to network...')
-        wlan.active(True)                       # Activate network interface
         # set power mode to get WiFi power-saving off (if needed)
         wlan.config(pm = 0xa11140)
         wlan.connect(keys.WIFI_SSID, keys.WIFI_PASS)  # Your WiFi Credential
         print('Waiting for connection...', end='')
 
         # Check if it is connected otherwise wait
-        while not wlan.isconnected() and wlan.status() >= 0:
-            print('.', end='')
-            sleep(1)
+        for _ in range(20):
+            if wlan.isconnected():
+                break
     
-    if wlan.isconnected():
+    if wlan.isconnected() and wlan.status() == 0:
         ip = wlan.ifconfig()[0]
         print('\nConnected on {}'.format(ip))
         return ip
